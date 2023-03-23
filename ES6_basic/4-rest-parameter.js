02c94f33c469564468e02e9238baf779df67275f
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2023-03-23 12:59:07 -0300

    task4 fix

--- ES6_basic/4-rest-parameter.js
+++ ES6_basic/4-rest-parameter.js
@@ -1,9 +1,3 @@
 export default function returnHowManyArguments(...theArgs) {
-
-  let total = 0;
-
-  for (const args of theArgs) {
-    total += args;
-  }
-  return total;
+  return theArgs.length;
 }
