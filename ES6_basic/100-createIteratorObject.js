855da2f6500f07d444165477f8e34afe05edc044
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2023-03-23 15:29:51 -0300

    task100

---
+++ ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,12 @@
+export default function createIteratorObject(report) {
+  const empleados = [];
+  /* eslint-disable*/
+  for (const [department, employees] of Object.entries(report.allEmployees)) {
+    for (const employe of employees) {
+      empleados.push(employe);
+    }
+  }
+  /* eslint-enable*/
+
+  return workers;
+}
--- main.js
+++
@@ -1,3 +0,0 @@
-import { taskFirst, taskNext } from './ES6_basic/0-constants.js';
-
-console.log(`${taskFirst()} ${taskNext()}`);
