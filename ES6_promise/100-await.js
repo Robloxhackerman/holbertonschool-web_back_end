b0903cfec71f994643ff383e6a490cd92b0d5a7f
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2023-03-23 22:42:02 -0300

    task100 + main

---
+++ ES6_promise/100-await.js
@@ -0,0 +1,20 @@
+import { uploadPhoto, createUser } from './utils';
+
+async function asyncUploadUser() {
+  let photo;
+  let user;
+
+  try {
+    photo = await uploadPhoto();
+    user = await createUser();
+  } catch (e) {
+    photo = null;
+    user = null;
+  }
+
+  const uploadUser = { photo, user };
+
+  return uploadUser;
+}
+
+export default asyncUploadUser;
