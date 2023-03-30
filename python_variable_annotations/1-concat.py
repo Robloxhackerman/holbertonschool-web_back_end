ea325cfdd5fa726e9de696216be245fa03d7084f
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2023-03-30 13:36:52 -0300

    task1 + main

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Write a type-annotated function concat that takes
+a string str1 and a string str2 as arguments and
+returns a concatenated string
+"""
+
+def concat(str1: str, str2: str) -> str:
+    """
+
+    :param str1:
+    :param str2:
+    :return:
+    """
+    return str1 + str2
