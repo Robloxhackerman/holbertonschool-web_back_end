bf0f5c5a65bbd814375e51a885e4ace135934306
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2023-03-30 14:51:27 -0300

    task8 + main

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Write a type-annotated function make_multiplier
+that takes a float multiplier as argument and
+returns a function that multiplies a float by multiplier.
+"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+
+    :param multiplier:
+    :return:
+    """
+    return lambda x: x * multiplier
