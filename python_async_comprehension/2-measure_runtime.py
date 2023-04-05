5d799aad261ce65fbce9ac3ef80f536d5ad682bb
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2023-04-05 16:44:02 -0300

    task2 + pepocho

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 """
+Import async_comprehension from the previous file and write a
+measure_runtime coroutine that will execute async_comprehension
+four times in parallel using asyncio.gather.
 
+measure_runtime should measure the total runtime and return it.
 """
 import asyncio
 import time
