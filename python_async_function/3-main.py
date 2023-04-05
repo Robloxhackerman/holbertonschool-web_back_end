92e537be8667c072e77bdf1f27ea52e131eba525
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2023-04-05 03:09:11 -0300

    task3 + main

--- python_async_function/3-main.py
+++ python_async_function/3-main.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python3
-"""
-Write a function (do not create an async function, use the regular
-function syntax to do this) task_wait_random that takes an integer
-max_delay and returns a asyncio.Task.
-"""
+
 
 import asyncio
 
@@ -11,11 +7,6 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def test(max_delay: int) -> float:
-    """
-
-    :param max_delay:
-    :return:
-    """
     task = task_wait_random(max_delay)
     await task
     print(task.__class__)
