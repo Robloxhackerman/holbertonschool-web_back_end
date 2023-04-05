b24e3b00b6d09259bbb9b3f579f18bb9a1a0be45
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2023-04-05 14:43:57 -0300

    task2

---
+++ MySQL_Advanced/2-fans.sql
@@ -0,0 +1,4 @@
+-- Write a SQL script that ranks country origins of bands,
+-- ordered by the number of (non-unique) fans
+
+SELECT origin, SUM(fans) AS nb_fans FROM metal_bands GROUP BY 1 ORDER BY 2 DESC
