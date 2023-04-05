9e33e0dc2d00a51e12de172d555b3dc28e141d00
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2023-04-05 16:26:40 -0300

    task0

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 """
-
+Write a coroutine called async_generator that takes no arguments.
 """
 import asyncio
 import random
 
 
 async def async_generator():
+    """
+
+    :return:
+    """
     for i in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
