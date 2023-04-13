1f2a3d5e5be43337488a32fb4ee484b8a0403cf2
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2023-04-13 15:35:26 -0300

    task10 + mian

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -5,6 +5,13 @@
 
 
 def update_topics(mongo_collection, name, topics):
+    """
+
+    :param mongo_collection:
+    :param name:
+    :param topics:
+    :return:
+    """
     return mongo_collection.update_many(
         {
             "name": name
