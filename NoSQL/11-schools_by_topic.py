da88d8ab51e0bee8784340d945898725078c66db
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2023-04-13 15:48:17 -0300

    task11 + mian

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Write a Python function that returns the list of school having a specific topic
+"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+
+    :param mongo_collection:
+    :param topic:
+    :return:
+    """
+    return list(mongo_collection.find(topic))
