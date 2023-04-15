ed37fff645574cf3f401fc31a45f4056e3a3491b
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2023-04-14 21:40:16 -0300

    task1 + main

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -3,7 +3,7 @@
 
 """
 import uuid
-from typing import Union
+from typing import Union, Optional, Callable
 
 import redis
 
@@ -29,3 +29,14 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+    def get(self, key: str, fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        data = self._redis.get(key)
+        if fn:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> str:
+        return str(self._redis.get(key),'utf-8')
+
+    def get_int(self, key: str) -> int:
+        return int(self._redis.get(key))
