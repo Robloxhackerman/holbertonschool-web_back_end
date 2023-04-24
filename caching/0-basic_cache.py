31c4c209ce3c166486106343aa97eca754841698
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2023-04-24 13:30:58 -0300

    task0 + main

---
+++ caching/0-basic_cache.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+
+    """
+
+    def put(self, key, item):
+        """
+
+        :param key:
+        :param item:
+        :return:
+        """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+
+        :param key:
+        :return:
+        """
+        if key is not None and key in self.cache_data:
+            return self.cache_data[key]
+        else:
+            return None
