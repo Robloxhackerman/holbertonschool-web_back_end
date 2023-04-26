384b9adf3380fc32a999c02170acdb2d55d53225
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2023-04-25 21:31:56 -0300

    task1

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -16,3 +16,12 @@ class TestAccessNestedMap(TestCase):
     def test_access_nested_map(self, map, path, expected_output):
         real_output = access_nested_map(map, path)
         self.assertEqual(real_output, expected_output)
+
+    @parameterized.expand([
+        ({}, ("a",), 'a'),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, map, path, wrong_output):
+        with self.assertRaises(KeyError) as e:
+            access_nested_map(map, path)
+            self.assertEqual(wrong_output, e.exception)
