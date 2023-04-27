32aa6d9ab7fe138b388fe767ecae4c9fbc5b7607
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2023-04-27 16:02:19 -0300

    task5

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -4,15 +4,17 @@ aaaaa
 """
 
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 
 from parameterized import parameterized
 from client import GithubOrgClient
+
 
 class TestGithubOrgClient(unittest.TestCase):
     """
     aaaa
     """
+
     @parameterized.expand([
         ('google'),
         ('abc')
@@ -28,3 +30,12 @@ class TestGithubOrgClient(unittest.TestCase):
         test_class = GithubOrgClient(input)
         test_class.org()
         mock.assert_called_once_with(f'https://api.github.com/orgs/{input}')
+
+    def test_public_repos_url(self):
+        """ Test the public repo url """
+        with patch('client.GithubOrgClient.org', new_callable=PropertyMock) as mock:
+            payload = {"repos_url": "World"}
+            mock.return_value = payload
+            test_class = GithubOrgClient('test')
+            result = test_class._public_repos_url
+            self.assertEqual(result, payload["repos_url"])
