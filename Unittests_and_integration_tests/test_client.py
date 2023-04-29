d81fc64b590d5411c63eee68d87f621b522f803b
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2023-04-28 21:39:59 -0300

    task7

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -65,3 +65,18 @@ class TestGithubOrgClient(unittest.TestCase):
 
             mock_public.assert_called_once()
             mock_json.assert_called_once()
+
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False)
+    ])
+    def test_has_license(self, repo, license_key, expected):
+        """
+
+        :param repo:
+        :param license_key:
+        :param expected:
+        :return:
+        """
+        result = GithubOrgClient.has_license(repo, license_key)
+        self.assertEqual(result, expected)
