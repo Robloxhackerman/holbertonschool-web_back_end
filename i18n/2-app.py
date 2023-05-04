8a02475b3c29d467a2bca6070cf7531a4780beb0
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2023-05-04 18:30:27 -0300

    task2

--- i18n/2-app.py
+++ i18n/2-app.py
@@ -7,21 +7,36 @@ from flask_babel import Babel
 
 
 class Config():
+    """
+    aaaa
+    """
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
+
 
 app = Flask()
 app.config.from_object(Config)
 babel = Babel(app)
 
+
 @babel.localeselector
 def get_locale():
+    """
+
+    :return:
+    """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def index():
+    """
+
+    :return:
+    """
     return render_template('2-index.html')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
