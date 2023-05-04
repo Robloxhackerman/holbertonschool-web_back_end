26fff417859b48db2fc1fc9b01571b52ba75edd5
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2023-05-04 17:11:58 -0300

    task0 fix

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python3
+from os import getenv
 
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     return render_template('0-index.html')
+
+if __name__ == "__main__":
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port)
