1483c117723c3b29eb31dadbb9242084e1ade2cf
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2023-05-04 17:41:50 -0300

    task1

---
+++ i18n/1-app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config():
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index():
+    """
+
+    :return:
+    """
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
