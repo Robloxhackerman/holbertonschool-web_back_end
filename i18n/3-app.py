92a67820fd8273670436d2d66505842161d71d33
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2023-05-04 20:28:33 -0300

    task3 fix

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -20,12 +20,8 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
-
+@babel.localeselector
 def get_locale():
-    """
-
-    :return:
-    """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
