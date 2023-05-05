77a8c9821c3b1a95ae90d3384bbd684a720b03f1
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2023-05-05 02:04:33 -0300

    task0 pepocho

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python3
 """
-
+aaaaaa
 """
 import re
 from typing import List
 
 
 def filter_datum(fields: List[str], redaction: str,
-                 message: str, separator: str):
+                 message: str, separator: str) -> str:
+    """
+
+    :param fields:
+    :param redaction:
+    :param message:
+    :param separator:
+    :return:
+    """
     pepe = message
     for field in fields:
         pepe = re.sub(field + "=.*?" + separator,
