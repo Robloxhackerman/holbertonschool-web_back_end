f007227450a7303b2ba3aa603c9d2e327e854830
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2023-05-05 21:44:16 -0300

    task1 + main

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -4,6 +4,7 @@ aaaaaa
 """
 import re
 from typing import List
+import logging
 
 
 def filter_datum(fields: List[str], redaction: str,
@@ -21,3 +22,20 @@ def filter_datum(fields: List[str], redaction: str,
         pepe = re.sub(field + "=.*?" + separator,
                       field + "=" + redaction + separator, pepe)
     return pepe
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        return filter_datum(self.fields, self.REDACTION, super(
+                RedactingFormatter, self).format(record),self.SEPARATOR)
