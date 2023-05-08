49d81e711eb8614998ef12791dd316ff1c30fa73
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2023-05-07 23:47:00 -0300

    task6

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -16,3 +16,17 @@ def hash_password(password: str) -> bytes:
     pass_hashed = bcrypt.hashpw(pass_encoded, bcrypt.gensalt())
 
     return pass_hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+
+    :param hashed_password:
+    :param password:
+    :return:
+    """
+    valid = False
+    pass_encoded = password.encode()
+    if bcrypt.checkpw(pass_encoded, hashed_password):
+        valid = True
+    return valid
