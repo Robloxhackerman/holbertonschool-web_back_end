fb837f30c5c027a782cad7230095b80adbc8f688
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2023-05-14 20:21:31 -0300

    task0

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -3,7 +3,22 @@ const calculateNumber = require('./0-calcul');
 
 describe('testCaclculator', () =>{
   it('1 + 3', () =>{
-    const PEPE = calculateNumber(1, 3)
+    const PEPE = calculateNumber(1, 3);
     assert.equal(PEPE, 4);
-  })
-})
+  });
+
+  it('1 + 3.7', () => {
+    const PEPE = calculateNumber(1, 3.7);
+    assert.equal(PEPE, 5);
+  });
+
+  it('1.2, 3.7', () => {
+    const PEPE = calculateNumber(1.2, 3.7);
+    assert.equal(PEPE, 5)
+  });
+
+  it('1.5, 3.7', () => {
+    const PEPE = calculateNumber(1.5, 3.7);
+    assert.equal(PEPE, 6)
+  });
+});
