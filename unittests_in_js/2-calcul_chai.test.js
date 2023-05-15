8dbc1079ca88c7945fdd7639ed83af928b8095c9
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2023-05-14 21:37:05 -0300

    task2

---
+++ unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,21 @@
+const expect = require('chai').expect;
+const calculateNumber = require('./1-calcul');
+
+describe('testCalculatorOmega', () => {
+  it('SUM, 1.4, 4.5', () => {
+    const PEPE = calculateNumber('SUM', 1.4, 4.5);
+    expect(PEPE).to.equal(6);
+  });
+  it('DIVIDE, 1.4, 4.5', () => {
+    const PEPE = calculateNumber('DIVIDE', 1.4, 4.5);
+    expect(PEPE).to.equal(0.2);
+  });
+  it('SUBTRACT, 1.4, 4.5', () => {
+    const PEPE = calculateNumber('SUBTRACT', 1.4, 4.5);
+    expect(PEPE).to.equal(-4);
+  });
+  it('DIVIDE, 1.4, 0', () => {
+    const PEPE = calculateNumber('DIVIDE', 1.4, 0);
+    expect(PEPE).to.equal('Error')
+  });
+});
