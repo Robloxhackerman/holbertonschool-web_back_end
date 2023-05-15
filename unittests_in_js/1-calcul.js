d3bcda1c18d023a3005afb1116f53f30061ff44e
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2023-05-14 21:08:16 -0300

    task1

---
+++ unittests_in_js/1-calcul.js
@@ -0,0 +1,15 @@
+function calculateNumber(type, a, b) {
+  switch (type) {
+    case 'SUM':
+      return Math.round(a) + Math.round(b);
+    case 'SUBTRACT':
+      return Math.round(a) - Math.round(b);
+    case 'DIVIDE':
+      if (b === 0){
+        return 'Error';
+      }
+      return Math.round(a) / Math.round(b);
+  }
+}
+
+module.exports = calculateNumber;
