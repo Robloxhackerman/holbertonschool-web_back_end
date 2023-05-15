32915da1f3c2ff15a84e27803b5efb39ea5ff37b
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2023-05-14 22:49:20 -0300

    task2 no... no puede ser aaaaaaaaaaaaaaaa

--- unittests_in_js/2-calcul_chai.test.js
+++ unittests_in_js/2-calcul_chai.test.js
@@ -1,32 +1,23 @@
-const chai = require('chai').expect;
+const expect = require('chai').expect;
 const calculateNumber = require('./2-calcul_chai');
 
 describe('testCalculatorOmega', () => {
-  describe('aaa', () => {
-    it('SUM, 1.4, 4.5', () => {
-      expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
-    });
-    it('SUM, 1.4, 4.5', () => {
-      expect(calculateNumber('SUM', 5, 4.5)).to.equal(10);
-    });
+  it('SUM, 1.4, 4.5', () => {
+    expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
+  });
+  it('SUM, 1.4, 4.5', () => {
+    expect(calculateNumber('SUM', 5, 4.5)).to.equal(10);
   });
 
-  describe('aaa', () => {
-    it('DIVIDE, 1.4, 4.5', () => {
-      expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
-    });
+  it('DIVIDE, 1.4, 4.5', () => {
+    expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
   });
 
-  describe('aaa', () => {
-    it('SUBTRACT, 1.4, 4.5', () => {
-      expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
-    });
+  it('SUBTRACT, 1.4, 4.5', () => {
+    expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
   });
-
-  describe('aaa', () => {
-    it('DIVIDE, 1.4, 0', () => {
-      chai.expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error')
-    });
+  it('DIVIDE, 1.4, 0', () => {
+    expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error')
   });
 
 });
