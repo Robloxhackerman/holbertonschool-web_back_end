fbebc08de756b14e7ff250b9fe3755f4be64ea2c
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2023-05-14 23:18:02 -0300

    task3 xd

--- unittests_in_js/3-payment.js
+++ unittests_in_js/3-payment.js
@@ -1,4 +1,4 @@
-const Utils = require('utils')
+const Utils = require('./utils.js')
 function sendPaymentRequestToApi(totalAmount, totalShipping){
   console.log(Utils.calculateNumber('SUM', totalAmount, totalShipping));
 }
