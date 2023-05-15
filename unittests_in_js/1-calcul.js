12ee64e81faf7716626c1be7d55d8d1399dac3a8
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2023-05-14 22:18:44 -0300

    task2 test

--- unittests_in_js/1-calcul.js
+++ unittests_in_js/1-calcul.js
@@ -5,7 +5,7 @@ function calculateNumber(type, a, b) {
     case 'SUBTRACT':
       return Math.round(a) - Math.round(b);
     case 'DIVIDE':
-      if (b === 0){
+      if (Math.round(b) === 0){
         return 'Error';
       }
       return Math.round(a) / Math.round(b);
