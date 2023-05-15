94e33261977140632f7694020fdc07081aad37fb
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2023-05-14 22:40:34 -0300

    task2 ._.

--- unittests_in_js/2-calcul_chai.test.js
+++ unittests_in_js/2-calcul_chai.test.js
@@ -1,83 +1,82 @@
 var chai = require('chai');
 const calculateNumber = require('./1-calcul');
 
-describe('testCalculatorOmega', () => {
+describe('calculateNumber', function () {
   describe('SUM no Round', function () {
     it('should return 5', function () {
       chai.expect(calculateNumber('SUM', 1, 4)).to.equal(5);
     });
   });
 
-  describe('SUM first round', () => {
-    it('should return 6', () => {
+  describe('SUM first round', function () {
+    it('should return 6', function () {
       chai.expect(calculateNumber('SUM', 2.4, 4)).to.equal(6);
     });
   });
 
-  describe('SUM second round ', () => {
-    it('should return 6', () => {
+  describe('SUM second round ', function () {
+    it('should return 6', function () {
       chai.expect(calculateNumber('SUM', 4, 2.4)).to.equal(6);
     });
   });
 
-  describe('SUM both round', () => {
-    it('aaa', () => {
+  describe('SUM both round', function () {
+    it('should return 6', function () {
       chai.expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
     });
   });
 
-  describe('SUBTRACT no round', () => {
-    it('aaa', () => {
+  describe('SUBTRACT no round', function () {
+    it('should return 2', function () {
       chai.expect(calculateNumber('SUBTRACT', 5, 3)).to.equal(2);
     });
   });
 
-  describe('SUBTRACT first round', () => {
-    it('aaa', () => {
+  describe('SUBTRACT first round', function () {
+    it('should return -3', function () {
       chai.expect(calculateNumber('SUBTRACT', 2, 4.5)).to.equal(-3);
     });
   });
 
-  describe('SUBTRACT second round', () => {
-    it('aaa', () => {
+  describe('SUBTRACT second round', function () {
+    it('should return 3', function () {
       chai.expect(calculateNumber('SUBTRACT', 4.5, 2)).to.equal(3);
     });
   });
 
-  describe('SUBTRACT both round', () => {
-    it('aaaa', () => {
+  describe('SUBTRACT both round', function () {
+    it('should return -4', function () {
       chai.expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
     });
   });
 
-  describe('DIVIDE no round', () => {
-    it('aaaa', () => {
+  describe('DIVIDE no round', function () {
+    it('should return 2', function () {
       chai.expect(calculateNumber('DIVIDE', 8, 4)).to.equal(2);
     });
   });
 
-  describe('DIVIDE first round', () => {
-    it('aaa', () => {
+  describe('DIVIDE first round', function () {
+    it('should return 5', function () {
       chai.expect(calculateNumber('DIVIDE', 9.5, 2)).to.equal(5);
     });
   });
 
-  describe('DIVIDE second round', () => {
-    it('aaa', () => {
+  describe('DIVIDE second round', function () {
+    it('should return 0.2', function () {
       chai.expect(calculateNumber('DIVIDE', 2, 9.5)).to.equal(0.2);
     });
   });
 
-  describe('DIVIDE both round', () => {
-    it('aaa', () => {
+  describe('DIVIDE both round', function () {
+    it('should return 0.2', function () {
       chai.expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
     });
   });
 
-  describe('DIVIDE Error', () => {
-    it('aaa', () => {
+  describe('DIVIDE Error', function () {
+    it('should return Error', function () {
       chai.expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
     });
   });
-
 });
