589a880b8b555c13589b1e8067d17e9d3a8dc1ad
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2023-05-21 17:30:01 -0300

    task5

---
+++ queuing_system_in_js/5-subscriber.js
@@ -0,0 +1,22 @@
+import {createClient} from "redis";
+
+const client = createClient();
+
+client.on('error', err => {
+  console.log('Redis client not connected to the server: ', err);
+});
+
+client.on('connect', ()=> {
+  console.log('Redis client connected to the server');
+});
+
+client.suscriber('holberton school channel');
+
+client.on('message', (channel, message) => {
+  console.log(message)
+  
+  if (message === 'KILL_SERVER') {
+    client.unsubscribe(channel);
+    process.exit(0);
+  }
+})
