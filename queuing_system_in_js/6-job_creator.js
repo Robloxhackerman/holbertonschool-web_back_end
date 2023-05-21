a60dc86e39526275e5520143be13be2fe48776d9
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2023-05-21 18:01:42 -0300

    task6

---
+++ queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,15 @@
+import kue from 'kue';
+const queue = kue.createQueue();
+
+const jobData = {
+  phoneNumber: '01010011',
+  message: 'sexy lady'
+}
+
+const job = queue.create('push_notification_code', jobData)
+    .save((error) => {
+      if (!error) console.log('Notification job created: ' + job.id);
+    });
+
+job.on('complete', () => console.log('Notification job completed'));
+job.on('failed', () => console.log('Notification job failed'));
