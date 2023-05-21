9e9e605f2a22afee721aebd39b1434ae37e79a9e
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2023-05-21 15:57:17 -0300

    task0

---
+++ queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,10 @@
+import { createClient } from 'redis';
+
+const client = createClient();
+
+client.on('connect', function() {
+  console.log('Redis client connected to the server');
+});
+client.on('error', err => {
+  console.log('Redis client not connected to the server: ', err)
+});
