80f7a5767d468791b45c9fa1892a36bded907245
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2023-06-02 22:51:37 -0300

    task6

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -10,3 +10,18 @@ class BasicAuth(Auth):
     """
     aaaaaa
     """
+
+    def extract_base64_authorization_header(self, authorization_header: str) \
+            -> str:
+        """
+
+        :param authorization_header:
+        :return:
+        """
+
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if "Basic " not in authorization_header:
+            return None
