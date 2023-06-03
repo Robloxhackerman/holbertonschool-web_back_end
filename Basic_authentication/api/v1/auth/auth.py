a5d9510d5488bbafd5c7ddff28cebbfd15f5d8a1
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2023-06-02 21:36:38 -0300

    task3

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -4,16 +4,33 @@ aaaa
 """
 from typing import TypeVar, List
 
-from flask import request
-
 
 class Auth:
+    """
+    aaaaaaaa
+    """
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+
+        :param path:
+        :param excluded_paths:
+        :return:
+        """
         return False
 
     def authorization_header(self, request=None) -> str:
+        """
+
+        :param request:
+        :return:
+        """
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
+        """
+
+        :param request:
+        :return:
+        """
         return None
