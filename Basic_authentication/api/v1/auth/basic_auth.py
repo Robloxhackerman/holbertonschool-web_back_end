f8c4b134311aab5e137dbb7ed24103fee3e42716
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2023-06-02 22:55:03 -0300

    task8

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """
 aaaaaa
 """
+import base64
 
 from api.v1.auth.auth import Auth
 
@@ -11,7 +12,7 @@ class BasicAuth(Auth):
     aaaaaa
     """
 
-    def extract_base64_authorization_header(self, authorization_header: str)\
+    def extract_base64_authorization_header(self, authorization_header: str) \
             -> str:
         """
 
@@ -27,3 +28,22 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header.split("Basic ", 1)[1]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """
+
+        :param base64_authorization_header:
+        :return:
+        """
+
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+
+        try:
+            return base64.b64decode(
+                base64_authorization_header).decode('utf-8')
+        except Exception:
+            return None
