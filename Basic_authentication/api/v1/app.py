5aa67b10e1a8efcd8c373cb4f48431a875072e5e
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2023-06-02 21:18:27 -0300

    task1

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -20,6 +20,14 @@ def not_found(error) -> str:
     """
     return jsonify({"error": "Not found"}), 404
 
+@app.errorhandler(401)
+def not_auth(error) -> str:
+    """
+
+    :param error:
+    :return:
+    """
+    return jsonify({"error": "Unauthorized"}), 401
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,12 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', strict_slashes=False)
+def error_401() -> None:
+    """
+
+    :return:
+    """
+    abort(401)
