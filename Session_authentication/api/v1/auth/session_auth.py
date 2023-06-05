abcf784178ee8863fc52f5d0aebbbc2ae666d163
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2023-06-04 21:22:36 -0300

    task6

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -5,6 +5,7 @@ aaaa
 import uuid
 
 from api.v1.auth.auth import Auth
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -16,7 +17,11 @@ class SessionAuth(Auth):
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
-        """ Creates a Session ID for user_id """
+        """
+
+        :param user_id:
+        :return:
+        """
 
         if user_id is None or isinstance(user_id, str) is False:
             return None
@@ -27,9 +32,24 @@ class SessionAuth(Auth):
         return session_id
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
-        """ Returns User ID based on Session ID """
+        """
+
+        :param session_id:
+        :return:
+        """
 
         if session_id is None or isinstance(session_id, str) is False:
             return None
 
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """
+
+        :param request:
+        :return:
+        """
+
+        session_id = self.session_cookie(request)
+
+        return User.get(self.user_id_for_session_id(session_id))
