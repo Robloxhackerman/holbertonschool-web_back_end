7c21cf6f9c32e486246cfba018072b9bac06937f
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2023-06-04 21:08:59 -0300

    task3

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -25,3 +25,11 @@ class SessionAuth(Auth):
         self.user_id_by_session_id[session_id] = user_id
 
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """ Returns User ID based on Session ID """
+
+        if session_id is None or isinstance(session_id, str) is False:
+            return None
+
+        return self.user_id_by_session_id.get(session_id)
