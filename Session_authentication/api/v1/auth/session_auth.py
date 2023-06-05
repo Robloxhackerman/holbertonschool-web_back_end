b8893b3ddf078ad7c6945c76d073ed6aa8b89d35
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2023-06-04 21:04:55 -0300

    task2

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -11,3 +11,16 @@ class SessionAuth(Auth):
     aaaaa
     """
     pass
+
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ Creates a Session ID for user_id """
+
+        if user_id is None or isinstance(user_id, str) is False:
+            return None
+
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+
+        return session_id
