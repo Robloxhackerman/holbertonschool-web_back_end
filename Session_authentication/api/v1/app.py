0b820f0a690eb53f512ed4ad162803ebb73e34ea
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2023-06-04 21:19:12 -0300

    task5

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -32,11 +32,14 @@ def request_filter() -> None:
     """
     excluded_paths = ['/api/v1/status/',
                       '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
+                      '/api/v1/forbidden/'
+                      '/api/v1/forbidden/',
+                      '/api/v1/auth_session/login/']
 
     if auth:
         if auth.require_auth(request.path, excluded_paths):
-            if auth.authorization_header(request) is None:
+            if auth.authorization_header(request) is None and \
+                    auth.session_cookie(request) is None:
                 abort(401)
             request.current_user = auth.current_user(request)
             if auth.current_user(request) is None:
